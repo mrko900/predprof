a4e2c5eb63f0829b2c6d24a2574b97fd4b3a818b
Nikolas-Kravtsov <63307366+Nikolas-Kravtsov@users.noreply.github.com>
2024-03-31 13:51:25 +0300

    Update code_for_db.py

--- code_for_db.py
+++ code_for_db.py
@@ -3,14 +3,12 @@ import sqlite3
 connect = sqlite3.connect("Building_in_date")
 cursor = connect.cursor()
 
-d = {}
+d = dates.dates
 mas = d.keys()
-k = 1
-for i in range(d.keys().size()):
-    cursor.execute(f"""INSERT INTO Dates (IDdate, years, months, days) values ({i + 1}, {mas[i][2]}, {mas[i][1]}, {mas[i][0]})""")
+for i in range(len(d.keys())):
+    cursor.execute(f"""INSERT INTO Dates (years, months, days) values ({mas[i][2]}, {mas[i][1]}, {mas[i][0]})""")
     l = d[mas[i]]
     for t in range(l.size()):
         for j in range(l[t].size()):
-            cursor.execute(f"""INSERT INTO Windows (IDwindow, Light, Room, Floor) 
-            values ({k}, {l[t][j][1]}, {l[t][j][0]}, {t + 1})""")
-            k += 1
+            cursor.execute(f"""INSERT INTO Windows (Light, Room, Floor, IDdate) 
+            values ({l[t][j][1]}, {l[t][j][0]}, {t + 1}, {i})""")
