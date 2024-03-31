66bfef9bbd81db146347312ad7a6ffcd5f3d3421
Nikolas-Kravtsov <63307366+Nikolas-Kravtsov@users.noreply.github.com>
2024-03-31 14:20:56 +0300

    Update db_funcs.py

--- db_funcs.py
+++ db_funcs.py
@@ -17,3 +17,27 @@ def push_to_db():
         i += 1
     connect.commit()
 
+def get_all_days():
+    connect = sqlite3.connect("db.sqlite3")
+    cursor = connect.cursor
+    a = cursor.execute("select count(*) from dates").fetchall()[0]
+    res = {}
+    for i in range(a):
+        k = cursor.execute("select count(distinct floor) from windows").fetchall()[0]
+        sp = []
+        for j in range(k):
+            mas = cursor.execute(f"""SELECT light, room from windows where IDdate = {i} and floor = {j + 1}""").fetchall()
+            sp.append(mas)
+        res[i] = sp
+
+def get_one_days(day, month, year):
+    connect = sqlite3.connect("db.sqlite3")
+    cursor = connect.cursor
+    a = cursor.execute(f"select id from dates where year = {year} and month = {month} and day = {day}").fetchall()[0]
+    res = []
+    k = cursor.execute(f"select count(distinct floor) from windows where IDdate = {a}").fetchall()[0]
+    for j in range(k):
+        mas = cursor.execute(f"""SELECT light, room from windows where IDdate = {a} and floor = {j + 1}""").fetchall()
+        res.append(mas)
+    res[i] = sp
+
