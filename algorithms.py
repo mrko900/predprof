fab5bee4769e51d864fb31d3cc40530fdc752dcf
mrko900 <mrko900@hotmail.com>
2024-03-31 11:46:53 +0300

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,13 @@
 class Date:
     def __init__(self, data):
         self.data = data  # список списков кортежей
+        self.rooms = 0
+        prev_room = -1
+        for fl in data:
+            for wnd in fl:
+                if wnd[0] != prev_room:
+                    prev_room = wnd[0]
+                    self.rooms += 1
 
     def get_light_room(self):
         res = []
@@ -10,3 +17,16 @@ class Date:
                     res.append(self.data[i][j][0])
         return res
 
+    def full_check_request(self, request):
+        res = [True for i in range(request.size())]
+        real_nums = self.get_light_room()
+        for i in range(request.size()):
+            res[i] = request[i] in real_nums
+        return res
+
+    def check_request(self, request):
+        res = True
+        real_nums = self.get_light_room()
+        for i in range(request.size()):
+            res *= request[i] in real_nums
+        return res
